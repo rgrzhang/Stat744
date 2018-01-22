5ece96ee95b0c16ee8b695d84b6999e49cc39bbe
rgrzhang <35642578+rgrzhang@users.noreply.github.com>
2018-01-22 14:25:05 -0500

    RgrZhang

--- hw1.R
+++ hw1.R
@@ -1,19 +1,9 @@
-3+3
-log10(100)
-Library(help="datasets")
-5+6
-factorial(5)
-data(package = .packages(all.available = TRUE))
-
-install.packages("ggplot2")
-install.packages("tidyverse")
 library(ggplot2)
 library(base)
 
-ggplot(data=Immunotherapy, mapping=aes(x=Type, y=Number_of_Warts, colour=Type))+
-  geom_point()+
-  facet_wrap(~sex)
 
 ggplot(data=Immunotherapy, mapping=aes(x=Type, fill=factor(Immunotherapy$Result_of_Treatment)))+
-  geom_bar()
-  
+  geom_bar()+
+  scale_fill_discrete(name="Success/Failure",
+                      breaks=c(0,1),
+                      labels=c("Failure","Success"))
