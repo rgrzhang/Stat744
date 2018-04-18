1ca51b7410545ff9a1a8974f9b4926377d66d421
Ben Bolker <bbolker@gmail.com>
2018-04-17 21:21:22 -0400

    BMB homework comments

--- HM2/hw2.R
+++ HM2/hw2.R
@@ -3,6 +3,11 @@
 
 library(ggplot2)
 
+## BMB: your code should be self-contained.
+
+vaccine_data_online <- read.csv("vaccine_data_online.csv")
+
+## BMB: colour="red" should be outside the aes() statement
 ggplot(data=vaccine_data_online,mapping=aes(x=year,y=cases))+
   geom_line()+
   facet_wrap(~disease,scale="free")+
@@ -12,10 +17,11 @@ ggplot(data=vaccine_data_online,mapping=aes(x=year,y=cases))+
 #The line plot is better at showing the trend.
 #Now we can see the effect of vaccine easier just by looking at how much the function decreases.
 
-
 ggplot(data=vaccine_data_online,mapping=aes(x=year,y=disease,size=cases,fill="red",alpha=cases))+
   geom_point(shape=20,colour="red")+
   scale_size_area(max_size = 15)+
   scale_x_continuous(breaks=c(1945:2015, by=10))
 
-#By plotting it horizontally, we can now see the whole picture instead of needing to scroll down every time.
+##By plotting it horizontally, we can now see the whole picture instead of needing to scroll down every time.
+
+## BMB several good points here.
--- High Dimensional HW/face plot.R
+++ High Dimensional HW/face plot.R
@@ -1,7 +1,14 @@
+## BMB: avoid file names with spaces in them!
 library(aplpack)
+
+## BMB: good practice to cache a version of the data set in case
+##  the original source goes away (or no network connection)
 crime <- read.csv("http://datasets.flowingdata.com/crimeRatesByState-formatted.csv")
 crime[1:6,]
 
 faces(crime[,2:8])
 crime_filled <- cbind(crime[,1:6], rep(0, length(crime$state)), crime[,7:8])
 faces(crime_filled[,2:8], labels=crime_filled$state)
+## BMB: does alphabetical order make sense?
+##  is there a way to add a legend? printing out the associations separately
+##  is OK, but seems clunky
