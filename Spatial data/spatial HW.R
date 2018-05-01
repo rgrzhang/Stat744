60f262cf6ebb36b5e6f99c6af0ecdebd4e4d47db
rgrzhang <35642578+rgrzhang@users.noreply.github.com>
2018-05-01 18:04:43 -0400

    Rgrzhang

--- Spatial data/spatial HW.R
+++ Spatial data/spatial HW.R
@@ -11,16 +11,18 @@ library(maps)
 
 leaflet(options = leafletOptions(minZoom = 0, maxZoom = 15))
 
-mapStates = map("states", fill = TRUE, plot = FALSE)
-leaflet(data = mapStates) 
+StatesofUS = map("states", fill = TRUE, plot = FALSE)
+leaflet(data = StatesofUS) 
 addTiles()
 for (i in 1:length(vary_p))
-  spatialpolygons(fillColor = i, stroke = FALSE)
+  spatialpolygons(fillColor = i)
   ## colours are used to do a fill in different states with spatial polygons.
 
 ## Now do a plot for weather. 
-leaflet()  addTiles() %>% setView(-100, 50, zoom = 5)
-  ## Zoom=5 should be a proper size for this plot, the area selected for plotting is approximated with where the country is located.
+leaflet()  
+addTiles()
+setView(-100, 50, zoom = 5)
+  ## The area selected for plotting is approximated with where the country is located.
   addWMSTiles(
     "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi",
     layers = "nexrad-n0r-900913",
